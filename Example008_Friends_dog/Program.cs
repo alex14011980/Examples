bb16ffe841cfcd7d1ad0785387c95307a340daaf
Alexander Bedrin <alexton83@mail.ru>
2023-09-01 20:33:09 +0400

    add Example008 Friends dog

---
+++ Example008_Friends_dog/Program.cs
@@ -0,0 +1,27 @@
+﻿int distance = 10000;
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 5;
+int count = 0;
+int time = 0;
+int friend = 2;
+
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        time = distance / (secondFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    
+    if(friend == 2)
+    {
+        time = distance / (firstFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+     
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * time;
+    count = count + 1;
+}
+Console.Write("Собака пробежит ");
+Console.WriteLine(count + " раз");
