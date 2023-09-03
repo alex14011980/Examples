fc06324bc250f761cfbb014d32f1ce497a79bfbf
Alexander Bedrin <alexton83@mail.ru>
2023-09-03 20:00:45 +0400

    add Example011 MethodArray

---
+++ Example011_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {11, 52, 91, 44, 75, 61, 27, 18, 91};
+
+int n = array.Length;
+int find = 91;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+
+    index++; 
+}
